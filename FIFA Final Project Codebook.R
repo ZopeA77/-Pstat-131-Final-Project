13583adce773b8d27c17c50fdedfc6d72377df75
Abhay Zope <102694157+ZopeA77@users.noreply.github.com>
2022-06-08 18:09:30 -0700

    Add files via upload

---
+++ FIFA Final Project Codebook.R
@@ -0,0 +1,117 @@
+FIFA Data Codebook
+
+This Kaggle dataset 
+(<https://www.kaggle.com/datasets/stefanoleone992/fifa-20-complete-player-dataset>) 
+contains player data from FIFA 15 to FIFA 20. 
+The dataset consists of 18,278 rows and 104 columns.
+Each row contains a unique player (observation) and each column contains unique 
+information about the player such as their name, age, height, weight, nationality, 
+and much more. All variables are stated and defined below.
+
+sofifa_id: unique number on sofifa
+player_url: url of the scraped player
+short_name: short name of the player
+long_name : long name of the player
+age: age of the player
+dob: date in which the player was born
+height_cm: height in cm of the player
+weight_kg: weight in kg of the player
+nationality: nationality of the player
+club: club of the player
+overall: overall rating of the player
+potential: potential highest rating of the player
+value_eur: value in EUR of the player
+wage_eur: wage in EUR of the player
+player_positions: positions of the player
+preferred_foot: preferred foot of the player
+international_reputation: international reputation of player
+weak_foot: weak foot attribute 
+skill_moves: quality moves attribute
+work_rate: productivity attribute
+body_type: type of body of the player (lean, average etc.)
+real_face: is the real_face of the player in the game
+release_clause_euro: release clause in Euros
+player_tags: player tags
+team_position: player position in the team
+team_jersey_number: player shirt number in the team
+loaned_from: club player was loaned from 
+joined: date player joined from club
+contract_valid_until: expiration date
+nation_position: player position in the national team
+nation_jersey_number: player shirt number in the national team
+pace: player pace
+shooting: player shooting attribute
+passing: player passing attribute
+dribbling: player dribbling attribute
+defending: player defending attribute
+physic: player physical attribute
+gk_diving: goalkeeper diving attribute
+gk_handling: goalkeeper handling attribute
+gk_kicking: goalkeeper kicking attribute
+gk_reflexes: goalkeeper reflex attribute
+gk_speed: goalkeeper speed attribute
+gk_positioning: goalkeeper positioning attribute
+player_traits: player_traits attribute
+attacking_crossing: player crossing attribute
+attacking_finishing: player finishing attribute
+attacking_heading: player heading attribute
+attacking_short_passing: player short passing attribute
+attacking_volleys: player air shot attribute
+skill_dribbling: player dribbling attribute
+skill_curve: player curve attribute
+skill_fk_accuracy: player fk accuracy attribute
+skill_long_passing: player long passing attribute
+skill_ball_control: player ball control attribute
+movement_acceleration: player acceleration attribute
+movement_sprint_speed: player sprint speed attribute
+movement_agility: player agility attribute
+movement_reactions: player reaction attribute
+movement_balance: player balance attribute
+power_shot_power: player shot power attribute
+power_jumping: player power jumping attribute
+power_stamina: player stamina attribute
+power_strength: player strength attribute
+power_long_shots: player long shots attribute
+mentality_aggression: player mentality attribute
+mentality_interceptions: player confidence in ability to intercept attribute
+mentality_positioning: player confidence in ability to position themselves attribute
+mentality_vision: player vision attribute
+mentality_penalties: player confidence in ability to score a penalty attribute
+mentality_composure: player composure attribute
+defending_marking: player defending attribute
+defending_standing_tackle: player standing tackle attribute
+defending_sliding_tackle: player sliding tackle attribute
+goalkeeping_diving: gk diving attribute
+goalkeeping_handling: gk handling attribute
+goalkeeping_kicking: goalkeeping kicking attribute
+goalkeeping_positioning: goalkeeping positioning attribute
+goalkeeping_reflexes: goalkeeping reflexes attribute
+ls: left striker value
+st: striker value
+rs: right striker value
+lw: left wing value
+lf: left forward value
+cf: center forward value
+rf: right forward value
+rw: right wing value
+lam: left attacking midfield value
+cam: center attacking midfield value
+ram: right attacking midfield value
+lm: left midfield value
+lcm: left center midfield value
+cm: center midfield value
+rcm: right center midfield value
+rm: right midfield value
+lwb: left wing back value
+ldm: left defensive midfield value
+cdm: center defensive midfield value
+rdm: right defensive midfield value
+rwb: right wing back value
+lb: left back value
+lcb: left center back value
+cb: center back value
+rcb: right center back value
+
+
+
+
